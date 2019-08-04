e26f1d227a27ddb3fb5acffc184eeb8c55df7f01
VoidChips <voidchips@gmail.com>
2019-08-04 00:14:45 -0500

    fresh pepe

---
+++ pepe.js
@@ -0,0 +1,106 @@
+const ascii_art = `
+NNNNNNNNNNNNNWNNNNNNNNNNNNNNNNNNNNWNWNNNNNNNNNNNNNNNWNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNWNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+NNNNNNNNNNNNNWNNNNNNNNNNNNNNNNNNNNNWNNNNNNNNNNNNNNWNNNNNNNNNNNNNNWNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+NNNNNNNNNNNKkoc:ccccccccccccccccc::cokKNNNNNNNNNN0dc:ccccccccc:cldONNNNNNWNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+NNWNNNNWKd:,'',;;;;::;;;;;;;;;;;;;;,'.,cxKWNNNNNk,.,;:;;:;;::;;;,.':clkXNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+NNNNNN0o,.,;:;;;;;;;;;;;;;;;;;;;;::;::;'.;OX0xc;,';:;;;;;;;;;;;;;;;;,'',l0NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+NNNNKo'.,;;;;;;;;;;;;;;;;;;;;;;;:;;::;;:;..,''';;:;;;;;;;;;::;;;;;;;;::,.,d0XNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+NNKo,.,;;;;;;;;;;;;;;;;;;:;;;;;::;;;;;;;;;'.';:;;;;;;;;;;;;;;;;;;;:;;:;;;,..:ONNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+Nk,.,::;::;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;'.':;;;;;;;:;;;;;;;;;;;;;;;;;;:;,'.oXWNNNNNWNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+x'';;::;;::;;;;:;;;;:::::;;;;;;;;;;;;;;;;;;;..;;;;;;;:;;;::;;;;;;;;;;;;;;::;;,.:KNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+'';;;:;;;;;;;;;;;;;,''.''''''''''''''''''''...,;::;;;;;;;;;;;;;::::::::;;;::;;,.:KNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+,;;;;:;;;;;;;;;;'...''',;;;;::::::::::::::;;,....,;;:;::;;,,'''''''''''',;;::;:,.cXWNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;:;;;;:;,..';;;;;;;;;;;;;;;;;;;;;;;;;;;;;,...',''....''',,,,,,,,'...''''','.oNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;:;,..';:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:,....',;;:::;;;;;;;;;;;;;;,,''....xNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;::;;;;;:;;;;;;:;:;;;:;;;;;;;;;;;;;;;;;::;;;;::;;;'...;:;;::;;;;:;;;;;;;;;;;:;;,..;d0NWNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;:;;;;;;;;:;;:;;;;;;;;;;;;;:;;;;;;;;;;;;;;;;;,..,::::;;;;;;;;;;;;;:::;;;;;,'.':xKWNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:;;;;;;;;;,..,::;;;;;;;;;;:;;;:::;;;;;;;:;'.;kXNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;;;;;;;;;;:;;;;;;;;;;;::::::::::::;:::::::;;;:,..,:;;;;;;;;;;;;;::::;;;;;;;;::;'.:0NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;;;;;;;;::;,,,,,,,,,,'''..''.....'''''''''''',;;..,;:;;;;;;;;;;;;;;;;;::::::::::;.'oKNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;:;;;;,'....''''''''''.................',,,'....'..,:;;::;;:;;;;:;;,,'''''''''''''..;dOXNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;::;;;;;;;;:;...',;;;;,,',,,'''''''''''',;;,,'....';;:;,.....;:;;:;;;;;;:;;,''........'''''''..';oKNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;:::;;;;;;'..,;,''...''''''',,,,,,,,,''....',::;,..';:;;;'..,;;;;;:;;;;:;,''...','',,,,,,,,,'....'lOXNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;:;;;;;'..';,...',;;::::;;;;;;:;;;;;;;;;;,...',;;'...';;:;'..,:;;;::;'....',''..''''''''''.',;'...'kWNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;:,..';;...,::;;;;;;;;;;;;;:;;;;::::;;;:;''...''''...,;:,.';;;:;'..',;,'...',,;;;;;;;;,,...;;.'xKNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;:'.';,..':;,,,,,,,,,,,,,,,,,'''......',,,,,,'.. ..'..';;..,;;;;',;,.....',,,,,,,,,,,;;::,..,':KWNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;:;'.'......,clodddddoc' .''.          ,xO00000Oxl'.,,..,,.';;;;:;..''.         .;ooc,'',,'  .:KWNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;::;;;:;;'',:lldOXWNo.  ,ko.     ...  .cKMMMMMMMMK:.;;..'.';;;;'.co' .::.       .cKWKkdoc,...,kWNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;::;;;;;;;:;;,''',ll.    ..    .x0X0,  ,0MMMMMMMMMO'.:;..';;;,.,dd'  .cc.    ;oo' oWMMMMWXkl..xWNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;;;;;;;;;;;:::,...    'c.   .xK0o.  '0MMMMMMMMMWd.':;..,:;.,0O.          '0MWc cNMMMMMMNd..xWNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;;;;;;;;;..',;::;,'.  ..     ...    :XMMMMMMMMMMNd.,:,',;,.lNx.    ':.   .;l:. lWMMMMNkc..'dWNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;;;;;;;;;;'...,:;;;;;,,'....       .kWMMMMMMMMWX0O;..';;;..lOo.              .,kXXK0x:.',',kWNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;;;;;;;:;;;;,...',;:::;;;:;;,....  .okOOOOOOOdc,,,'...':;,,','...............':c:;;,'';'':kNWNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;:;;;;;;;::;;;;'...'''',,;;;;;:;;;,,'',,,,,,,',;::;,..';;;;;::::;;;;;;;;;;;;;;;;::::;'.'xNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;;::;;;;;;;;;;;;:;,,,'''..'''''''''',,,,,,,,,,,'...'',;;:;'.....',,;;;;;;;;;;;,''''.. .kNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;:::;;;;;;;;;;;;;;;;;;;;:;;;;:::;;;;;;,,,,'''''''''''...',;::;;:;;;;'......''''',;;;;:;,,,'....;xXNNNNWNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;:;;,;;:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;::;::::;'..,;;:;;:;:;;;;;:;'....',;;;::;;,'''''.';;'.lXNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;'..',;;;;;;;;;;;;;;;;;;;;;;;;;;;;;::;;;:;;;;,...';:;;:;;::;;;;;;;;;;;;,...,;:;:;;;;;;;;;;;:,.cKWNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+:;;;;;;;;;::::;;;;;;;;;;;;;;;;;;;;;;;;;;::;;;::;;'...,;:;;;::;;;;;;;;;;;;;;:;;;,...,;;;;;;;::;;;;;:,.cKNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;:;;;:;,'''''';:;;;;;;;;;;;;;;;;;;;;;;;;;::;,''...',;;;:;;;;;:;;;;;;;;;;;;;;;;;;;,..,;;;:;;;;;;:;;:;..'xXNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+:;;;'....,,,,,;:;;;;;;;;;:;;;;;:;;;;;:;,,'...'',,;:;;;;;;;;:;;;;;;;;;;;;;;;;;:;;:;;;;;;;;;;;;;;;;;:;....oNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;:;..''...'''''',;;:;;;:;;;;;:;;;;;;;,'.''',;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;::;;;;;;;;'.';.;0WNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;,;,..,,'''.......',;;;;::;;;;;;;;;;;;:::;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;'..,'.:KWNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;:,..,,,,'..,,,'......''''',;;;;:::;;;:;;;;;:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,..,..oKNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;'...',,'.....,,,,''..'.......''.',;:::;;;;;;;;;:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;::;;;,'..'.,kNNNNNNNWWNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;'....',,'.....',,,,,,,,,,,,''.....''''',,,;;;:::::::;;;;;;;;;;;;;;;;;;;;;;;;;;;::::;,.......,0WNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;,....,,,'........''',,,,,,,,,,'''.........''''''''''''''''''''''''''''''''''',''...','. ,ONNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;::;;;'.....',,,,''..........''',,,,,,,,,;;,,,,''''''...'''''''''''''''''''''''''''',,'....;0WNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;;;;::;,'......',,,,,,,''...................''',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,''....','.dNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;;;;;;;;::;,,'....',,,,,,,,,,,,,,,,,,,,'''.........................................',,,,'.oNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;;;;;;;:;;;;;;::,....',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,..cKNNWNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;;;;;;;;;;;;;::;;;;'......',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,'.;kNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;;;;;;:;;;;;;::::;;;;;;,'......''',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,'..;KWNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;::;;,'''''.......'''''',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,''.....'.;KWNNWWNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;::::::;;;;,,'''''''''''''''''''''''''''''''''''''..'',;;;.lXWNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;;;;;;;;::;;;;;;;;;;;;;;;;;;::;;;;;;;;;;:::;::::::::::::::::::::::::::::;;;;:::;;;;;',OWNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:;;;;::;;;;;;;;;::,'......,;::::,.dNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;::;;;;;;;;::;,...,;;;,'.....',dXNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:;,'....',;;;;;::;'...'OWNNNNWWNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:;;;;;'...'',;:;;;;;:;;;;;;,.;oOXNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;::::;;;:;;'..';;;:;;;:;;;;;;;;;;:;;,'';cx0NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:;;;;;;;,''''',;:;'...';::;;:;;;:;;,,;;::;:;;;:;,'',:dXNNNNWNNWNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:;;,...,,,'.....','..,:;;;:;;,.....',',;::,',;::;,';d0NNNNNNNWNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:;;;;:,..,;;;;:,..';;;;'.'::;;,'..',.'o0d..,'...;:;;:;;,''oXNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:::;..,;;;;:;'..;;;'.';,....'';'.:0N0:....',;;:;;;;;::''lKNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:;;:;;,..';;;;;;,..''..''.',,:;'':xXNk' ..,;;;;;;;;;;;;;;,.,dXNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;::;;;;;;,...;;;;;::;;..,::;;,'';xKxl:'',;;:;;;::::;;;;:;;;;..;kXNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:;;:;;;;;:;,..';;;;;;:,.',',;:lxdc;'',;:;;;;;;;:::;;;;;;;;;:;''.;dXNNWNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:;;:;;;;;;:;..,;;;;;:;..:xKXNO:.';;:;;;;;;;;;,.';:;;;;;::;;:;:;'.dNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:;..;::;;;;'.:ONNk'.;:;:;;;;;;;;,...,;:;;:;:;;;;;;;;;',kNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:,.':;;;;;;,.':l'.,;;;;;:;;;:;...,;;;:::::;;,;;;:;;;;''c0NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:;;;;;;:;;:,.':;;;;::;;;'..  .,;;;;;:;,..,;;;;;;:;;:'..,:;;;;:;:,.lXNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:;..:;;;;;;;:;;:;......,;::,..;:;;;;;;;;'..';:;;:;,,;;:':KWNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:;,...;:;:;;;;;;;;;::;;'...',..;:;:;;;;;'..,;;;;;:;..,:;:,.dNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:;..ld,.;:;;;:;;;;;;;;;;;;,'',..';;;;;;;..';;;;;;;,..,;;;::''kNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:;;;;;;:;,..dNNx.':;;;;:;;;;;;;;;;;;;;:;..,;;;:;..';;;;;;;...,;;::;:;.lXNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+`;
+
+let lines = [];
+// remove newline character
+for (char of ascii_art) {
+	if (char !== '\n') {
+		lines[lines.length - 1] += char;
+	}
+	else {
+		lines.push('');
+	}
+}
+
+let i = 0;
+// print new line after waiting
+const displayMeme = () => {
+	console.log('Created by VoidChips\n');
+	setInterval(() => {
+		if (i < lines.length) {
+			console.log(lines[i]);
+			i++;
+		}
+		else {
+			console.log('https://github.com/VoidChips/Pepe');
+			// quit program after printing done
+			process.exit();
+		}
+	}, 80);
+}
+displayMeme();
